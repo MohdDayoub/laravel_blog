ce3cc898b4e440a877609fd08d7116ebbf99fa55
Nour-Alsakka <nour8alsakka@gmail.com>
2024-07-24 11:59:56 +0300

    show image from storage/media

--- resources/views/admin/authors/index.blade.php
+++ resources/views/admin/authors/index.blade.php
@@ -2,26 +2,25 @@
 
 @section('main')
 
-    <a class="btn btn-secondary" href="{{ url('/dashboard/authors/create') }}">اضافة كاتب</a>
+<a class="btn btn-secondary" href="{{ url('/dashboard/authors/create') }}">اضافة كاتب</a>
 
 
-    <table class="table">
+<table class="table">
 
-        <tr>
-            <th scope="col">الصورة</th>
-            <th scope="col">الاسم</th>
-            <th scope="col">الوصف</th>
-        </tr>
+    <tr>
+        <th scope="col">الصورة</th>
+        <th scope="col">الاسم</th>
+        <th scope="col">الوصف</th>
+    </tr>
 
-        @foreach($authors as $author)
-            <tr>
-                <td>{{$author->image}}</td>
-                <td>{{$author->name}}</td>
-                <td>{{$author->description}}</td>
-            </tr>
-        @endforeach
+    @foreach($authors as $author)
+    <tr>
+        <td><img src="{{url('storage/media/'.$author->image)}}" alt="" style="width:200px"></td>
+        <td>{{$author->name}}</td>
+        <td>{{$author->description}}</td>
+    </tr>
+    @endforeach
 
-    </table>
+</table>
 
 @endsection
-
