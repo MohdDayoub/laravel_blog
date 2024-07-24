2d9b3ce5e3d678b60d9ccad4a7ca695cc3b6a355
Nour-Alsakka <nour8alsakka@gmail.com>
2024-07-24 22:59:05 +0300

    add datatables

--- resources/views/admin/authors/index.blade.php
+++ resources/views/admin/authors/index.blade.php
@@ -5,22 +5,30 @@
 <a class="btn btn-secondary" href="{{ url('/dashboard/authors/create') }}">اضافة كاتب</a>
 
 
-<table class="table">
+<table id="myTable" class="display">
 
-    <tr>
-        <th scope="col">الصورة</th>
-        <th scope="col">الاسم</th>
-        <th scope="col">الوصف</th>
-    </tr>
+    <thead>
+        <tr>
+            <th scope="col">الصورة</th>
+            <th scope="col">الاسم</th>
+            <th scope="col">الوصف</th>
+        </tr>
+    </thead>
 
-    @foreach($authors as $author)
-    <tr>
-        <td><img src="{{url('storage/media/'.$author->image)}}" alt="" style="width:200px"></td>
-        <td>{{$author->name}}</td>
-        <td>{{$author->description}}</td>
-    </tr>
-    @endforeach
+    <tbody>
+        @foreach($authors as $author)
+        <tr>
+            <td><img src="{{url('storage/media/'.$author->image)}}" alt="" style="width:100px"></td>
+            <td>{{$author->name}}</td>
+            <td>{{$author->description}}</td>
+        </tr>
+        @endforeach
+    </tbody>
 
 </table>
-
+<script>
+    let table = new DataTable('#myTable', {
+        // config options...
+    });
+</script>
 @endsection
